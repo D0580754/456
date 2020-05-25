680c84f78b0fc097ee084b772236fd5c407c331c
D0580754 <32447524+D0580754@users.noreply.github.com>
2020-05-25 18:08:55 +0800

    Update app.py

--- app.py
+++ app.py
@@ -5,13 +5,9 @@ app = Flask(__name__) # __name__ 為 python 內建的變數，他會儲存目前
 
 
 @app.route("/") #函式的裝飾 Decorator，以底下函式為基礎，提供附加的功能，這邊 "/" 代表根目錄
-
 def home():
+    return "<h1>D0737806 李育嫻</h1>" 
 
-    return "Hello Flask12"
-
-    
 
 if __name__ == "__main__": #如果以主程式運行
-
-app.run()  #啟動伺服器 debug=True 修改內容將立即反應在網頁
+    app.run()  #啟動伺服器 debug=True 修改內容將立即反應在網頁
